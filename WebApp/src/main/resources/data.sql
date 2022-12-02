a8073ee3568d3d9e7ae034380be9d8176673dfe7
Rachel Mallie McDaniel <105060765+MalMcDan@users.noreply.github.com>
2022-12-02 08:13:40 -0500

    Add files via upload

---
+++ WebApp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1001,'Mallie', 'Get AWS Certified', CURRENT_DATE(), false );
+
+insert into todo(ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1002,'Mallie', 'Get A+ Certified', CURRENT_DATE(), false );
+
+insert into todo(ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1003,'Mallie', 'Get Project+ Certified', CURRENT_DATE(), false );
+
+insert into todo(ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1004,'Mallie', 'Get ITIL Certified', CURRENT_DATE(), false );
